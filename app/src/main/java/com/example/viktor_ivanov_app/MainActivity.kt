42ea2c89fc5ab43dcf257e074da8db5697bb7434
Viktorphp84 <viktorphp@yandex.ru>
2021-11-27 16:27:01 +0300

    27.11.2021

--- app/src/main/java/com/example/viktor_ivanov_app/MainActivity.kt
+++ app/src/main/java/com/example/viktor_ivanov_app/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.viktor_ivanov_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.viktor_ivanov_app.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var bindingClass:ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        bindingClass= ActivityMainBinding.inflate(this.layoutInflater)
+        setContentView(bindingClass.root)
+
+        //используем элементы
+        bindingClass.textView.text="ghgjg"
     }
 }
