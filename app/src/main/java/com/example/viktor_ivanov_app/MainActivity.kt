48ef755b8a4c90e08c262786e34ffc5accbe8a0e
Viktorphp84 <viktorphp@yandex.ru>
2021-11-29 02:11:52 +0300

    29.11.2021

--- app/src/main/java/com/example/viktor_ivanov_app/MainActivity.kt
+++ app/src/main/java/com/example/viktor_ivanov_app/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.example.viktor_ivanov_app
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import com.example.viktor_ivanov_app.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -13,7 +16,43 @@ class MainActivity : AppCompatActivity() {
         bindingClass= ActivityMainBinding.inflate(this.layoutInflater)
         setContentView(bindingClass.root)
 
+        //actionBar?.title="Ввод данных"
         //используем элементы
-        bindingClass.textView.text="ghgjg"
+        // bindingClass.textView.text="Здрасте!"
+    }
+
+    override fun onStart() {
+        Log.d("MyLog", "OnStart")
+        super.onStart()
+
+    }
+
+    override fun onPause() {
+        Log.d("MyLog", "OnPause")
+        super.onPause()
+    }
+
+    override fun onStop() {
+        Log.d("MyLog", "OnStop")
+        super.onStop()
+    }
+
+    override fun onRestart() {
+        Log.d("MyLog", "OnRestart")
+        super.onRestart()
+    }
+
+    override fun onDestroy() {
+        Log.d("MyLog", "OnDestroy")
+        super.onDestroy()
+    }
+
+    fun onClickTest(view: View) {
+        bindingClass= ActivityMainBinding.inflate(this.layoutInflater)
+        setContentView(bindingClass.root)
+        //bindingClass.button.text = "ПРЮВЕТ!"
+
+        val intent: Intent = Intent(this, ActivityInsertData::class.java)
+        startActivity(intent)
     }
 }
