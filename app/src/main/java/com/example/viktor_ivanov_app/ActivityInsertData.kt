35e3aff7ff1e7b28000ec1b5cd1f5dd0eb961773
Viktorphp84 <viktorphp@yandex.ru>
2021-12-16 23:12:48 +0300

    16.12.2021

--- app/src/main/java/com/example/viktor_ivanov_app/ActivityInsertData.kt
+++ app/src/main/java/com/example/viktor_ivanov_app/ActivityInsertData.kt
@@ -1,24 +1,17 @@
 package com.example.viktor_ivanov_app
 
-import android.content.Intent
-import android.net.Uri
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.PersistableBundle
-import android.text.Layout
-import android.view.View
-import android.view.ViewGroup
 import android.widget.Button
-import android.widget.EditText
-import android.widget.LinearLayout
-import android.widget.TextView
 import androidx.core.os.bundleOf
-import androidx.core.view.children
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import com.example.viktor_ivanov_app.databinding.ActivityMainBinding
+import androidx.fragment.app.Fragment
+
 
 private const val KEY = "number"
+private val CONSUM_FRAGMENT = InputFragmentConsum().javaClass.name
+private val COS_FRAGMENT = InputFragmentConsum().javaClass.name
+private val LENGTH_FRAGMENT = InputFragmentConsum().javaClass.name
 
 class ActivityInsertData : AppCompatActivity() {
 
@@ -34,8 +27,32 @@ class ActivityInsertData : AppCompatActivity() {
         val fragmentCos = InputFragmentCos()
         val fragmentLength = InputFragmentLength()
 
+        var fragment: Fragment? = null
+
+        when(supportFragmentManager.findFragmentById(R.id.fragment_container_view)) {
+            is InputFragmentConsum ->
+                fragment = if(savedInstanceState != null) {
+                    supportFragmentManager.getFragment(savedInstanceState, CONSUM_FRAGMENT)
+                } else {
+                    fragmentCos
+                }
+            is InputFragmentCos ->
+                fragment = if(savedInstanceState != null) {
+                    supportFragmentManager.getFragment(savedInstanceState, COS_FRAGMENT)
+                } else {
+                    fragmentCos
+                }
+            is InputFragmentLength ->
+                fragment = if(savedInstanceState != null) {
+                    supportFragmentManager.getFragment(savedInstanceState, LENGTH_FRAGMENT)
+                } else {
+                    fragmentLength
+                }
+            else -> fragment = fragmentConsum
+        }
+        
         supportFragmentManager.beginTransaction().
-        replace(R.id.fragment_container_view, fragmentConsum). //addToBackStack(fragment.tag).//при нажатии кнопки домой экраны запоминаются как в стеке
+        replace(R.id.fragment_container_view, fragment!!). //addToBackStack(fragment.tag).//при нажатии кнопки домой экраны запоминаются как в стеке
         commit()
 
         fragmentConsum.arguments = bundleOf(Pair("num", number))
@@ -43,15 +60,23 @@ class ActivityInsertData : AppCompatActivity() {
         buttonNextFragment = findViewById(R.id.button_next_frag)
 
         buttonNextFragment.setOnClickListener{
-            val fragment =
+            fragment =
                 when(supportFragmentManager.findFragmentById(R.id.fragment_container_view)) {
                     is InputFragmentConsum -> fragmentCos
                     is InputFragmentCos -> fragmentLength
                     else -> fragmentConsum
                 }
-            supportFragmentManager.beginTransaction().replace(R.id.fragment_container_view, fragment).commit()
+            supportFragmentManager.beginTransaction().replace(R.id.fragment_container_view, fragment!!).commit()
 
-            fragment.arguments = bundleOf(Pair("num", number))
+            fragment?.arguments = bundleOf(Pair("num", number))
         }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+
+        val currentFragment = supportFragmentManager.fragments.last()
+
+        supportFragmentManager.putFragment(outState, currentFragment.javaClass.name, currentFragment)
     }
 }
